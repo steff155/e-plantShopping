159127ad14c307e5f65cb5505b403a49ea5dec84
sk <steff15@gmx.de>
2025-08-28 14:18:32 +0200

    Deploy build to docs

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  base: "/shoppingreact",
   plugins: [react()],
-})
+  base: "/e-plantShopping/", // 👉 ganz wichtig für GitHub Pages
+});
